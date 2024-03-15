af539e1e580400b699096a4430a92a286f1bd12a
hasanur-dev <59501123+hasanur-dev@users.noreply.github.com>
2024-03-16 04:45:09 +0530

    Update initiateOAuth.js

--- functions/initiateOAuth.js
+++ functions/initiateOAuth.js
@@ -3,8 +3,9 @@
 exports.handler = async (event, context) => {
   console.log("initiateOAuth");
   const client_id = process.env.ACCESS_KEY;
+  const redirect_uri = process.env.REDIRECT_URI;
 
-  const authUrl = `https://unsplash.com/oauth/authorize?client_id=${client_id}&redirect_uri=http://localhost:8888/login&response_type=code&scope=public+read_user+write_user+read_photos+write_photos+write_likes+write_followers+read_collections+write_collections`;
+  const authUrl = `https://unsplash.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=public+read_user+write_user+read_photos+write_photos+write_likes+write_followers+read_collections+write_collections`;
 
   return {
     statusCode: 302,
