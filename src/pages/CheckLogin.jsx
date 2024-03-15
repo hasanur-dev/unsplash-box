75965fa4b82b901d91023de14d3ddb491ba7581b
hasanur-dev <59501123+hasanur-dev@users.noreply.github.com>
2024-03-16 05:04:07 +0530

    Update CheckLogin.jsx

--- src/pages/CheckLogin.jsx
+++ src/pages/CheckLogin.jsx
@@ -1,34 +1,32 @@
-import { useMutation } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { loginUser } from "../services/apiUnsplash";
 import { useUserContext } from "../contexts/UserContext";
+import Spinner from "../ui/Spinner";
 
 export default function CheckLogin() {
   const navigate = useNavigate();
 
   const [params, setSearchParams] = useSearchParams();
   const { setUser } = useUserContext();
-  const [userData, setUserData] = useState({});
   const code = params.get("code");
-  console.log(code);
-  const { mutate, error } = useMutation({
-    mutationFn: loginUser,
-    onSuccess: (data) => {
-      console.log("success");
-      console.log(data);
-      setUserData(data);
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => loginUser(code),
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
       setUser(data);
       navigate(-1);
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
-  useEffect(() => {
-    if (code) {
-      mutate(code);
     }
-  }, [code, mutate]);
-  return <div>CheckLogin</div>;
+  }, [isSuccess, navigate, setUser, data]);
+
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
 }
